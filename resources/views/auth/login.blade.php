c3256f8f4fec392473667390f0263791b83b026c
Ryan <fuhailims@gmail.com>
2023-05-09 17:44:51 +1000

    basic configuration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="container section">
+    <div class="row justify-content-center container">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
@@ -70,4 +70,44 @@
         </div>
     </div>
 </div>
+
+
+{{--<div class="account-login section">--}}
+{{--    <div class=container>--}}
+{{--        <div class=row>--}}
+{{--            <div class="col-lg-6 offset-lg-3 col-md-10 offset-md-1 col-12">--}}
+{{--                <form class="login-form" action="#" method='post' id="LF" >--}}
+{{--                    <div >--}}
+{{--                        <div class=title>--}}
+{{--                            <h3>Login</h3>--}}
+{{--                            <p>Login by entering the information below.</p>--}}
+{{--                            <div class="text-danger" id="logErr"></div>--}}
+{{--                        </div>--}}
+{{--                        <div class="form-group input-group">--}}
+{{--                            <label><img src="../html/assets/icon/email.png" height="25px" alt=""></label>--}}
+{{--                            <input class='form-control placeholder-color' type='email' placeholder="Enter your email" id="LE" required>--}}
+{{--                        </div>--}}
+{{--                        <div class="form-group input-group">--}}
+{{--                            <label><img src="../html/assets/icon/password.png" height="25px" alt=""></label>--}}
+{{--                            <input class='form-control placeholder-color' type='password' placeholder="Enter your password" id="LPW" required>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="d-flex flex-wrap justify-content-between bottom-content">--}}
+{{--                        <div class=form-check>--}}
+{{--                            <label for="LCK" class="hover-orange form-check-label">--}}
+{{--                                <input type="checkbox" class="form-check-input checkBox width-auto" id="LCK">--}}
+{{--                                <b>Remember me!</b>--}}
+{{--                            </label>--}}
+{{--                        </div>--}}
+{{--                        <a class=lost-pass href='./reset.html'>Forgot password?</a> </div>--}}
+{{--                    <div class='button'>--}}
+{{--                        <button class='btn' id="LG" type='submit'>Login</button>--}}
+{{--                    </div>--}}
+{{--                    <h4 class=create-account>Don't have an account? <a href='./registration.html'>Registration Here</a> </h4>--}}
+{{--                </form>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html class="no-js" lang="zxx">
 
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
@@ -13,6 +14,7 @@
     <link rel="stylesheet" href="{{asset("css/animate.css")}}" />
     <link rel="stylesheet" href="{{asset("css/tiny-slider.css")}}" />
     <link rel="stylesheet" href="{{asset("css/index.css")}}" />
+    <link rel="stylesheet" href="{{asset("css/login.css")}}" />
 
     <!-- ========================= JS here ========================= -->
     <script src="{{asset("js/jquery-3.6.4.min.js")}}"></script>
@@ -24,49 +26,114 @@
 
 <body>
 
-<!-- Start Header Area -->
-<header class="header navbar-area">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-12">
-                <div class="nav-inner">
-                    <!-- Start Navbar -->
-                    <nav class="navbar navbar-expand-lg">
-                        <a class="navbar-brand" href="index.html">
-                            <img src="assets/images/logo/logo.png" alt="Logo">
-                        </a>
-                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
-                            <ul id="nav" class="navbar-nav ms-auto">
-                                <li class="nav-item">
-                                    <a href="index.html" class="home" aria-label="Toggle navigation">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="./trading.html" aria-label="Toggle navigation">Trading</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="./masterTrading.html" aria-label="Toggle navigation">Master of Trading</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
-                                       data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false"
-                                       aria-label="Toggle navigation">Get started</a>
-                                    <ul class="sub-menu collapse" id="submenu-1-1">
-                                        <li class="nav-item"><a href="registration.html">Register now</a></li>
-                                        <li class="nav-item"><a href="login.html">Login/Logout</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="button">
-                            <input placeholder="Renewable energy type" class="search" id="search-input">
-                            <button id="search-button" class="search-button" >Search</button>
-                        </div>
-                    </nav>
-                    <!-- End Navbar -->
+<body>
+
+<div id="app">
+    <!-- Start Header Area -->
+    <header class="header navbar-area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <div class="nav-inner">
+                        <!-- Start Navbar -->
+                        <nav class="navbar navbar-expand-lg">
+
+                            <a class="navbar-brand" href="index.html">
+                                <img src="../../public/assets/images/logo/logo.png" alt="Logo">
+                            </a>
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                                <ul id="nav" class="navbar-nav ms-auto">
+                                    <li class="nav-item">
+                                        <a href="index.html" class="home" aria-label="Toggle navigation">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="./trading.html" aria-label="Toggle navigation">Trading</a>
+                                    </li>
+
+                                    @if(isset($status))
+                                    @if($status==1 && $data['role'] == "manager")
+                                        <li class="nav-item">
+                                            <a href="./masterTrading.html" aria-label="Toggle navigation">Master of Trading</a>
+                                        </li>
+                                    @endif
+                                    @endif
+
+                                    <li class="nav-item">
+                                        <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
+                                           data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent" aria-expanded="false"
+                                           aria-label="Toggle navigation">Get started</a>
+                                        <ul class="sub-menu collapse" id="submenu-1-1">
+                                            <li class="nav-item"><a href="registration.html">Register now</a></li>
+                                            <li class="nav-item"><a href="login.html">Login/Logout</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="button">
+                                <input placeholder="Renewable energy type" class="search" id="search-input">
+                                <button id="search-button" class="search-button" >Search</button>
+                            </div>
+
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <!-- Left Side Of Navbar -->
+                                <ul class="navbar-nav me-auto">
+
+                                </ul>
+
+                                <!-- Right Side Of Navbar -->
+                                <ul class="navbar-nav ms-auto">
+                                    <!-- Authentication Links -->
+                                    @guest
+                                        @if (Route::has('login'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                            </li>
+                                        @endif
+
+                                        @if (Route::has('register'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                            </li>
+                                        @endif
+                                    @else
+                                        <li class="nav-item dropdown">
+                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                {{ Auth::user()->name }}
+                                            </a>
+
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                                    {{ __('Logout') }}
+                                                </a>
+
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                    @csrf
+                                                </form>
+                                            </div>
+                                        </li>
+                                    @endguest
+                                </ul>
+                            </div>
+                        </nav>
+                        <!-- End Navbar -->
+                    </div>
                 </div>
-            </div>
-        </div> <!-- row -->
-    </div> <!-- container -->
-</header>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </header>
+    <!-- End Header Area -->
+
+    <main >
+        @yield('content')
+    </main>
+
+</div>
+</body>
 
 <!-- End Header Area -->
