d27a7cf9532102038dc4cad817f4093d1489065d
JohnnyZ111 <129571663+JohnnyZ111@users.noreply.github.com>
2023-05-13 12:47:15 +1000

    修改了profile部分错误，home中的轮播图2~3的路径更正了，在images文件夹下增加了profile文件夹增加了图片。

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/profile', function(){
-    return view('profile');
-})->name('profile');
-
-
 
 Auth::routes();
 
@@ -67,3 +62,8 @@ Route::get('/home', function () {
 Route::get('/', function () {
     return view('home');
 });
+
+
+Route::get('/profile', function(){
+    return view('profile');
+})->name('profile');
